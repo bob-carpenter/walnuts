acf1d59376c5f2a9f78dfa072b0fc65c4e5ab091
torekleppe <tore.kleppe@uis.no>
2025-06-18 13:32:44 +0200

    plots for Gaussian examples

--- WALNUTSpy_examples/gaussian/mainGaussTransient.py
+++ WALNUTSpy_examples/gaussian/mainGaussTransient.py
@@ -13,9 +13,11 @@ import matplotlib.pyplot as plt
 np.random.seed(seed=1)
 ds = 2**np.arange(11,16)
 nrep = 50
-nstep = 100
+nstep = 31
 
 res = np.zeros((nrep,len(ds),3,nstep+1))
+hmins = np.zeros((nrep,len(ds),3,nstep))
+hmaxs = np.zeros((nrep,len(ds),3,nstep))
 evals = np.zeros((nrep,len(ds),3,nstep+1))
 evalsF = np.zeros((nrep,len(ds),3,nstep+1))
 
@@ -42,6 +44,8 @@ for dd in ds:
                                       igrAux=ai.integratorAuxPar(),
                                       )
         res[rep,k,0,:] = samples[0,:]
+        hmaxs[rep,k,0,:] = H*(2**(-diagnostics[:,8]))
+        hmins[rep,k,0,:] = H*(2**(-diagnostics[:,9]))
         evals[rep,k,0,:] = np.r_[np.zeros(1),np.cumsum(diagnostics[:,6]+diagnostics[:,7])]
         evalsF[rep,k,0,:] = np.r_[np.zeros(1),np.cumsum(diagnostics[:,6])]
     
@@ -54,6 +58,8 @@ for dd in ds:
                                       igrAux=ai.integratorAuxPar(),
                                       )
         res[rep,k,1,:] = samples[0,:]
+        hmaxs[rep,k,1,:] = H*(2**(-diagnostics[:,8]))
+        hmins[rep,k,1,:] = H*(2**(-diagnostics[:,9]))
         evals[rep,k,1,:] = np.r_[np.zeros(1),np.cumsum(diagnostics[:,6]+diagnostics[:,7])]
         evalsF[rep,k,1,:] = np.r_[np.zeros(1),np.cumsum(diagnostics[:,6])]
         
@@ -67,11 +73,15 @@ for dd in ds:
                                       igrAux=ai.integratorAuxPar(),
                                       )
         res[rep,k,2,:] = samples[0,:]
+        hmaxs[rep,k,2,:] = H
+        hmins[rep,k,2,:] = H
         evals[rep,k,2,:] = np.r_[np.zeros(1),np.cumsum(diagnostics[:,6])]
         evalsF[rep,k,2,:] = np.r_[np.zeros(1),np.cumsum(diagnostics[:,6])]
 
         
 
     np.save("gaussTransientRes",res)
+    np.save("gaussTransientHmin",hmins)
+    np.save("gaussTransientHmax",hmaxs)
     np.save("gaussTransientEvals",evals)
     np.save("gaussTransientEvalsForw",evalsF)
